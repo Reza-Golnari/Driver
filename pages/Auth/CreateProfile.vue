b93d68fc2fa97ce5deb417094b24e52fe6dedb31
Reza <i.am.reza.programmer@gmail.com>
2024-01-20 21:10:42 +0330

    update assets

---
+++ pages/Auth/CreateProfile.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div
+      class="w-32 h-32 p-6 rounded-full flex items-center justify-center bg-primary"
+    >
+      <img src="/images/picture.png" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>

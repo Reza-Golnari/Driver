e21f54cdc7377ca31dbd17f89ee200d6d550c669
Reza <i.am.reza.programmer@gmail.com>
2024-01-21 19:04:12 +0330

    improv(history.vue): create history page

---
+++ pages/profile/history.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-4 min-w-80">
+    <SectionsBackButton />
+    <h2 class="text-end md:text-center md:text-3xl text-xl font-bold mb-5">
+      تاریخچه بارهای من
+    </h2>
+    <div class="flex items-center justify-center flex-col gap-y-5">
+      <LazySectionsBarCard
+        v-for="(card, index) in 4"
+        :key="index"
+        :data="{ callBtn: true }"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup></script>

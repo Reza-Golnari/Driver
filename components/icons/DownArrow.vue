2b2e2cb11d0648b9e0d61fd588413f19f97b734e
Reza <i.am.reza.programmer@gmail.com>
2024-01-22 21:15:48 +0330

    improv(cartype.vue): create base

---
+++ components/icons/DownArrow.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 512 512"
+  >
+    <path
+      fill="currentColor"
+      d="m367.997 338.75l-95.998 95.997V17.503h-32v417.242l-95.996-95.995l-22.627 22.627L256 496l134.624-134.623z"
+    ></path>
+  </svg>
+</template>
--- pages/Auth/CarType.vue
+++ pages/Auth/CarType.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    <img src="" alt="" />
+  <div class="h-screen min-w-80 flex flex-col items-center py-24 gap-y-4">
+    <!-- Back Button -->
+    <SectionsBackButton />
+    <img src="/images/car-type.png" />
+    <div class="w-full">
+      <div
+        class="bg-primary text-white w-full mx-auto flex items-center justify-between py-4 px-8 rounded-lg cursor-pointer"
+      >
+        <IconsDownArrow class="text-lg" />
+        انتخاب نوع ماشین
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup></script>
-
-<style lang="scss" scoped></style>

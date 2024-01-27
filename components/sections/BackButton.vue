223933f3df6ca0901d795a7e911e8402cecf1d28
Reza <i.am.reza.programmer@gmail.com>
2024-01-27 13:00:47 +0330

    map page

--- components/sections/BackButton.vue
+++ components/sections/BackButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute top-3 left-3 text-4xl cursor-pointer">
+  <div class="absolute top-3 left-3 text-4xl cursor-pointer z-20">
     <NuxtLink @click="$router.go(-1)">
       <IconsBackArrow />
     </NuxtLink>
---
+++ pages/profile/map.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="w-full md:max-w-[500px] flex flex-col items-center justify-between h-screen py-4"
+  >
+    <SectionsBackButton />
+    <NeshanMap
+      :mapKey="mapKey"
+      :serviceKey="serviceKey"
+      :scale="0.5"
+      class="w-10/12 border h-72"
+    />
+    <div></div>
+    <SectionsBarCard :data="{ callBtn: false }" />
+  </div>
+</template>
+<script setup>
+const mapKey = process.env.MAP_KEY;
+const serviceKey = process.env.SERVICE_KEY;
+import NeshanMap from "@neshan-maps-platform/vue3-openlayers";
+</script>
